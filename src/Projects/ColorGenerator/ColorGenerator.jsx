2b4a5addffdbbc52a35e93f92481dab670976692
Rupesh3000 <rupeshpatil11121@gmail.com>
2024-12-29 08:11:20 +0000

    create only very besic and very easy Hex and rgb color generator

--- src/Projects/ColorGenerator/ColorGenerator.jsx
+++ src/Projects/ColorGenerator/ColorGenerator.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
 
 function ColorGenerator() {
-
-    const [typeOfColor, setTypeOfColor] = useState("hex")
     const [color, setColor] = useState("#00000")
 
 
@@ -17,21 +15,34 @@ function ColorGenerator() {
 
         for (let i = 0; i < 6; i++) {
             hexColor += hex[randomColorUitily(hex.length)]
-            
-        }
 
-        console.log(hexColor);
+        }
+        setColor(hexColor)
+    }
+
+
+    const handleCreateRandomRGBColor = () => {
+        const r = randomColorUitily(256)
+        const g = randomColorUitily(256)
+        const b = randomColorUitily(256)
+
+        setColor(`rgb(${r},${g},${b})`);
     }
     return (
-        <div className="wrapper flex justify-center bg-yellow-400">
+        <div className="wrapper flex justify-center bg-yellow-400 h-screen">
 
-            <div className=" container  justify-center bg-red-300">
+            <div className="justify-center bg-red-300 w-full h-screen" style={{ backgroundColor: color }}>
                 <div className='flex justify-center gap-3 mt-3'>
                     <button
                         onClick={handleCreateRandomHexColor}
-                        className='bg-yellow-200 px-4 py-3 text-xl rounded-lg'>HEX Generator</button>
-                    <button className='bg-yellow-200 px-4 py-3 text-xl rounded-lg'>RGB Color Generator</button>
-                    <button className='bg-yellow-200 px-4 py-3 text-xl rounded-lg'>Rendom Color Generator</button>
+                        className='bg-yellow-200 px-4 py-3 text-xl rounded-lg'>Generator Random HEX Color </button>
+                    <button
+                        onClick={handleCreateRandomRGBColor}
+                        className='bg-yellow-200 px-4 py-3 text-xl rounded-lg'>Generator Random RGB Color </button>
+                </div>
+
+                <div className='flex justify-center items-center text-7xl mt-24'>
+                    {color}
                 </div>
             </div>
 
