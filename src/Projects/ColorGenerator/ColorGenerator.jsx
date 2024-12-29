b06f0cc004bc252d6d80e7a7eb1d85bf3241141d
Rupesh3000 <rupeshpatil11121@gmail.com>
2024-12-29 09:26:33 +0000

    create star rating selector

--- src/Projects/ColorGenerator/ColorGenerator.jsx
+++ src/Projects/ColorGenerator/ColorGenerator.jsx
@@ -1,33 +1,43 @@
 import React, { useState } from 'react'
 
-function ColorGenerator() {
-    const [color, setColor] = useState("#00000")
+function ColorGenerator({ noOfStars = 5 }) {
+    const [color, setColor] = useState("Colors")
+    const [rating, setRating] = useState(0)
+    const [hover, setHover] = useState(0)
 
 
-    const randomColorUitily = (length) => {
-        return Math.floor(Math.random() * length)
-    }
+
+    // random color generator 
+    const randomColorUtility = (length) => Math.floor(Math.random() * length);
     const handleCreateRandomHexColor = () => {
-
-        let hex = [1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F',];
-
-        let hexColor = "#";
-
+        const hex = ['1', '2', '3', '4', '5', '6', '7', '8', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+        let hexColor = '#';
         for (let i = 0; i < 6; i++) {
-            hexColor += hex[randomColorUitily(hex.length)]
-
+            hexColor += hex[randomColorUtility(hex.length)];
         }
-        setColor(hexColor)
-    }
-
-
+        setColor(hexColor);
+    };
     const handleCreateRandomRGBColor = () => {
-        const r = randomColorUitily(256)
-        const g = randomColorUitily(256)
-        const b = randomColorUitily(256)
-
+        const r = randomColorUtility(256);
+        const g = randomColorUtility(256);
+        const b = randomColorUtility(256);
         setColor(`rgb(${r},${g},${b})`);
+    };
+
+
+    // star rating 
+
+    const handleRatingClick = (getCurrentIndex) => {
+        setRating(getCurrentIndex)
     }
+    const handleMouseEnter = (getCurrentIndex) => {
+        setHover(getCurrentIndex)
+
+    }
+    const handleMouseLeave = () => {
+        setHover(rating)
+    }
+
     return (
         <div className="wrapper flex justify-center bg-yellow-400 h-screen">
 
@@ -35,14 +45,32 @@ function ColorGenerator() {
                 <div className='flex justify-center gap-3 mt-3'>
                     <button
                         onClick={handleCreateRandomHexColor}
-                        className='bg-yellow-200 px-4 py-3 text-xl rounded-lg'>Generator Random HEX Color </button>
+                        className='bg-yellow-200 px-4 py-3 text-xl rounded-lg'>Generate Random HEX Color </button>
                     <button
                         onClick={handleCreateRandomRGBColor}
-                        className='bg-yellow-200 px-4 py-3 text-xl rounded-lg'>Generator Random RGB Color </button>
+                        className='bg-yellow-200 px-4 py-3 text-xl rounded-lg'>Generate Random RGB Color </button>
                 </div>
 
-                <div className='flex justify-center items-center text-7xl mt-24'>
+                <div className='flex justify-evenly items-center text-7xl mt-24'>
                     {color}
+
+                    <div className='starRating text-9xl flex gap-1'>
+                        {
+                            [...Array(noOfStars)].map((_, index) => {
+                                index += 1;
+                                return <h1
+                                    key={index}
+                                    className={index <= (hover || rating) ? 'text-yellow-400': 'text-black'}
+                                    onClick={() => handleRatingClick(index)}
+                                    onMouseEnter={() => handleMouseEnter(index)}
+                                    onMouseLeave={() => handleMouseLeave()}
+                                >
+                                    *
+                                </h1>
+                            })
+                        }
+                    </div>
+
                 </div>
             </div>
 
